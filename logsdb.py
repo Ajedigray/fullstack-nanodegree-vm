f22e63a389198eea04e025e40626b6a060a60622
Ajedigray <33495512+Ajedigray@users.noreply.github.com>
2017-12-02 20:37:16 -0500

    Add query 3 to logsdb.py

--- logsdb.py
+++ logsdb.py
@@ -49,12 +49,43 @@ def popularAuthors():
 		author = row[0]
 		print (bullet + '"' + author + '"' + ' - ' + num_of_views + views)
 	db.close()
-  
+
+
+def errorProne():
+	print(b + "\n3. On which days did more than 1% of requests lead to errors?" +
+							r)
+	db = psycopg2.connect(database=DBNAME)
+	c = db.cursor()
+	c.execute('''select date_part('month', time) as month,
+				date_part('day', time) as day, date_part('year', time) as year,
+				sum(case when status = '404 NOT FOUND' then 1 else 0 end) as error,
+				count(*) as total_requests from log group by month, day, year
+				order by day;''')
+	rows = c.fetchall()
+	months = {"1": "January", "2": "February", "3": "March",
+				"4": "April", "5": "May", "6": "June", "7": "July",
+				"8": "August", "9": "September", "10": "October",
+				"11": "November", "12": "December"}
+	for row in rows:
+		error = int(row[3])
+		total = int(row[4])
+		error_rate = error / total
+		
+		if error_rate > 0.01:
+			month = months['%d' % (row[0])]
+			day = '%d' % (row[1])
+			year = '%d' % (row[2])
+			error_rate_pct = str(round(error_rate*100, 2))
+			print (bullet + ' ' + month + ' ' + day + ', ' + year + ' - ' +
+					error_rate_pct + '% ' + 'errors\n')
+	db.close()
+
+
 def init():
 	header()
 	popularArticles()
 	popularAuthors()
-		
+	errorProne()
   
   
   init()
